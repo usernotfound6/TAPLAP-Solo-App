33a314e78a1839a53484ead138a73e5495f340ab
fraol <phrolaye@gmail.com>
2023-09-19 22:38:12 -0500

    test

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -4,7 +4,7 @@ import { Card, CardContent, Typography } from "@mui/material";
 // This is one of our simplest components
 // It doesn't have local state,
 // It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+// or even care what the redux state is.'
 
 function AboutPage() {
   return (
